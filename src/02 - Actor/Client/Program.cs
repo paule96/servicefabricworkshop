530824ce5d9e15d577638b11d44b59be0873c26e
Paul Jeschke <Paul-Jeschke@outlook.com>
2018-04-03 09:42:13 +0200

    sample for actors

---
+++ src/02 - Actor/Client/Program.cs
@@ -0,0 +1,42 @@
+﻿using GetNumber.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using RenderHtml.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            GetResult().GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        static async Task GetResult()
+        {
+            try
+            {
+                var getnumber = ActorProxy.Create<IGetNumber>(ActorId.CreateRandom(), new Uri("fabric:/_02__Actor/GetNumberActorService"));
+                await getnumber.SetCountAsync(5, new System.Threading.CancellationToken());
+                Task<int> number = getnumber.GetCountAsync(new System.Threading.CancellationToken());
+                var renderHtmlActor = ActorProxy.Create<IRenderHtml>(ActorId.CreateRandom(), new Uri("fabric:/_02__Actor/RenderHtmlActorService"));
+                await renderHtmlActor.SetContent(5, new System.Threading.CancellationToken());
+                Task<string> retval = renderHtmlActor.GetHtmlAsync(new System.Threading.CancellationToken());
+                var html = retval.GetAwaiter().GetResult();
+                Console.WriteLine(html);
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ src/02 - Actor/RenderHtml.Interfaces/IRenderHtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Remoting.FabricTransport;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+[assembly: FabricTransportActorRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+namespace RenderHtml.Interfaces
+{
+    /// <summary>
+    /// This interface defines the methods exposed by an actor.
+    /// Clients use this interface to interact with the actor that implements it.
+    /// </summary>
+    public interface IRenderHtml : IActor
+    {
+        /// <summary>
+        /// TODO: Replace with your own actor method.
+        /// </summary>
+        /// <returns></returns>
+        Task<string> GetHtmlAsync(CancellationToken cancellationToken);
+        Task<int> SetContent(int number, CancellationToken cancellationToken);
+    }
+}
